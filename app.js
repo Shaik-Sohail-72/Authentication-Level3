b258cc160ccb7d3ceccacbd46bc51396ef758acd
sohail <shaiksohailhu7n@gmail.com>
2022-05-27 16:33:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+require('dotenv').config()
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+const mongoose=require("mongoose");
+const md5 = require('md5');
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(process.env.LINK,{useNewUrlParser:true})
+const userSchema= new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+
+
+const User=new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res)
+{
+    res.render("home");
+});
+app.get("/login",function(req,res)
+{
+    res.render("login");
+});
+app.get("/register",function(req,res)
+{
+    res.render("register");
+});
+
+app.post("/register",function(req,res)
+{
+    const newUser=new User({
+        email:req.body.username,
+        password:md5(req.body.password)
+    });
+    newUser.save(function(err)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login",function(req,res)
+{
+    const username=req.body.username;
+    const password=md5(req.body.password);
+    User.findOne({email:username},function(err,foundUser)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            if(foundUser.password===password)
+            {
+                res.render("secrets");
+            }
+        }
+
+    });
+});
+
+
+app.listen(3000,function()
+{
+    console.log("server started on port 3000.");
+})
